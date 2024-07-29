3fa022f07aaa670717a6fbbe7c8c761858dd29c4
Rafael Gonçalo <94737578+RafaelG0NCALO@users.noreply.github.com>
2024-07-29 17:53:09 -0300

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/check-urls': {
+        target: 'https://apipythonmigration.vercel.app', // URL do seu backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/check-urls/, '/check-urls'),
+      },
+    },
+  },
+});
